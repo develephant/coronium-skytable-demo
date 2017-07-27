40083d3fe5775eec9672cb7b0854b08e2f11d4a2
develephant <code@develephant.com>
2017-07-27 09:45:28 -0500

    clean

---
+++ data.lua
@@ -0,0 +1,18 @@
+return {
+  profile = {
+    name = "Jimmy"
+  },
+  saves = {
+    save1 = {
+      score = 100,
+      level = 2,
+      bag = {
+        items = {
+          "sword",
+          "beaker"
+        },
+        gold = 2000
+      }
+    }
+  }
+}
---
+++ test.lua
@@ -0,0 +1,27 @@
+-- local data = 
+-- {
+--   colors = {"Red","Green","Blue","Purple","Orange","Yellow"},
+--   ages = {23,56,65,23,54,12,42}
+-- }
+
+--skytable:random("ages", networkListener)
+--skytable:slice("colors", 1, 4, networkListener)
+--skytable:trim("colors", 1, 2, networkListener)
+--skytable:remove("colors", 2, networkListener)
+--skytable:insert("colors", 1, {"Pink","Brown"}, networkListener)
+--skytable:unshift("colors", networkListener)
+--skytable:shift("colors", "Purple", networkListener)
+--skytable:pop("colors", networkListener)
+--skytable:push("colors", "Purple", networkListener)
+--skytable:indexOf("colors", "Blue", networkListener)
+--skytable:arrlen("colors", networkListener)
+--skytable:delete("score", networkListener)
+--skytable:len("name", networkListener)
+--skytable:keys(netListener)
+--skytable:strappend("name", ", Frank", networkListener)
+--skytable:strlen("name", networkListener)
+--skytable:mult("score", 2, networkListener)
+--skytable:dec("score", 2, networkListener)
+--skytable:inc("score", 2, networkListener)
+--skytable:set("score", 20, networkListener)
+--skytable:set("profile.name", "Johnson", netListener)
